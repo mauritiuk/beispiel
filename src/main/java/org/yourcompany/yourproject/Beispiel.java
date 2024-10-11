9c508f5a41ff5777fc7fb7e50d1790116f459094
Klaus Mauritius <klaus.mauritius@hansemerkur.de>
2024-10-11 13:57:54 +0200

    first commit

---
+++ src/main/java/org/yourcompany/yourproject/Beispiel.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.yourcompany.yourproject;
+
+/**
+ *
+ * @author mauritiu
+ */
+public class Beispiel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Weiterer Test");
+    }
+}
